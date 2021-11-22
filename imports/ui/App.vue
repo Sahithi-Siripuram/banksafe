f43ee086bb9f35a3d3723d95a78ffa745940a277
Sahithi Siripuram <sahithi.s@comakeit.com>
2021-11-22 10:06:43 +0530

    toggle nav working

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="container">
+  <div>
     <div class="nav" v-show="login">
       <Navbar
         v-for="element in elements1"
         :key="element._id"
         :element="element"
+        :login = "login"
+        :navid="navid"
+        v-on:update:login="login=$event"
+        v-on:update:navid="navid=$event"
       />
     </div>
     <div class="nav" v-show="!login">
@@ -12,36 +16,52 @@
         v-for="element in elements2"
         :key="element._id"
         :element="element"
+        :login = "login"
+        :navid="navid"
+        v-on:update:login="login=$event"
+        v-on:update:navid="navid=$event"
       />
     </div>
-    <div v-if="navid==='home'"></div>
-    <div v-else-if="navid==='myTransactions'"></div>
-    <div v-else-if="navid==='intiateTransactions'"></div>
-    <div v-else></div>
+    <div v-if="navid==='home'">
+      <Home/>
+    </div>
+    <div v-else-if="navid==='myTransactions'">
+      <h1>History</h1>
+    </div>
+    <div v-else-if="navid==='intiateTransaction'">
+      <h1>New Transaction</h1>
+    </div>
+    <div v-else>
+      <h1>My profile</h1>
+    </div>
   </div>
 </template>
 
 <script>
   import Navbar from "./components/Navbar.vue"
+  import Home from "./components/Home.vue"
   import "./assets/styles.css"
   export default {
       components: {
         Navbar,
+        Home,
       },
-      data : {
-          login: true,
-          navid: "home",
-          elements1:  [
-          { _id: 1, text: "Home" },
-          { _id: 2, text: "My Transactions" },
-          { _id: 3, text: "Initiate Transaction"},
-          { _id: 4, text: "My Profile"},
-          { _id: 5, text: "Logout"}
-        ],
-        elements2: [
-        { _id: 1, text: "Home" },
-        { _id: 2, text: "Login / Signup"}
-      ],
+      data () {
+          return{
+            login: true,
+            navid: "home",
+            elements1:  [
+              { _id: 1, text: "Home" },
+              { _id: 2, text: "My Transactions" },
+              { _id: 3, text: "Initiate Transaction"},
+              { _id: 4, text: "My Profile"},
+              { _id: 5, text: "Logout"}
+            ],
+            elements2: [
+                  { _id: 1, text: "Home" },
+                  { _id: 5, text: "Login / Signup"}
+            ],
+          }; 
       },
       methods: {},
   };
--- imports/ui/components/Navbar.vue
+++ imports/ui/components/Navbar.vue
@@ -4,12 +4,23 @@
 
 <script>
   export default {
-      props: ["element"],
+      props: ["element","navid","login"],
       data() {
       return {};
   },
   methods:{
-    
+    display(){
+      if(this.element._id===1)
+        this.$emit('update:navid',"home");
+      else if(this.element._id===2)
+        this.$emit('update:navid',"myTransactions");
+      else if(this.element._id===3)
+        this.$emit('update:navid',"intiateTransaction");
+      else if(this.element._id===4)
+        this.$emit('update:navid',"myProfile");
+      else
+        this.$emit('update:login',!this.login);
+    }
   }
 };
 </script>
