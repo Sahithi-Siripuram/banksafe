eb6b8ffce55813ea9de1e716a72578fc52a8697a
Sahithi Siripuram <sahithi.s@comakeit.com>
2021-11-22 10:23:38 +0530

    home

--- imports/ui/components/Home.vue
+++ imports/ui/components/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-      <h1>Hello World</h1>
+      <h1>Welcome to BankSafe Banking solutions!</h1>
+      <p>We are India's first blockhain banking solution. With us on your side, be rest assured your data is absolutely safe.
+        Your transactions are now, safer, faster and you may now bid goodbye to all the transaction costs.
+      </p>
   </div>
 </template>
 
